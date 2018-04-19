b797bf637dcc6f88c406782d13de0dd35d7fc4fd
Lz <2120140200@mail.nankai.edu.cn>
2018-04-19 18:02:33 +0800

    update
    A simple speckle simulation algorithm

--- example.m
+++ example.m
@@ -1,22 +1,33 @@
 
-g=double(imread('C:\Users\lz\Desktop\example.bmp')); %%%%%%%%%%%%%%%%%%%%%%%set as your pic path
-g=g(:,:,1);
-%%%%%%%%%%%%%从0-2pi生成随机相位矩阵%%%%%%%%%%%%%%%
-TarImg=normrnd(0,2*pi,size(g,1),size(g,2)); 
 
-%%%%%%%%%%%%%将随机相位矩阵与目标物体结合%%%%%%%%%%%
- IP0=g.*exp(1i*TarImg);
 
-%%%%%%%%%%%%%通过傅里叶变换生成散斑图%%%%%%%%%%%
- speckle=abs(fftshift(fft2(IP0,500,500))).^2;
+%%%%%%%%%%%%%%set as your pic or just the 
+g=double(imread('\path\to\example.bmp')); 
 
- %%%%%%%%%%%%%%%归一化%%%%%%%%%%%%%%%
- speckle=speckle./max(max(speckle(:,:)));
- figure
- imshow(speckle)
+%%%%%%%%%%%%%% a simple way to produce a binary pattern from the pic
+g=g(:,:,1); 
+
+%%%%%%%%%%%%%%generate random phase from 0-2pi
+RandomPhase=normrnd(0,2*pi,size(g,1),size(g,2)); 
+
+%%%%%%%%%%%%%%combine the binary pattern with the random pahse 
+IP0=g.*exp(1i*RandomPhase);
+
+%%%%%%%%%%%%%%produce the speckles pattern through a FFT transform
+speckle=abs(fftshift(fft2(IP0,500,500))).^2;
+
+%%%%%%%%%%%%%%simple norm the data,
+speckle=speckle./max(max(speckle(:,:)));
+ 
+%%%%%%%%%%%%%%show , the speckle produced by the binary pattern,
+%%%%%%%%%%%%%%and in real it means optical capture
+figure
+imshow(speckle)
  
  
- result=abs(fftshift(fft2(speckle)));%%%%%%%%%%%%%%%%%%%%%%%%%调整对比度
+%%%%%%%%%%%%%% one time of FFT transform to get the correlation and then
+%%%%%%%%%%%%%% in this pattern you can see the shape of the original pattern
 
- 
- imtool(result)
+%%%%%%%%%%%%%%%%%%%%%%%%%caution锛侊紒锛乽 should adjust the contrast in imtool to see the result
+result=abs(fftshift(fft2(speckle)));%%%%%%%%%%%%%%%%%%%%%%%%%
+imtool(result)
